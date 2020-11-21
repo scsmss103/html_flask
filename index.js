3aa19b78b90d5be79b79a42414ad28371e693115
scsmss103 <s.schlumpf@gmail.com>
2020-11-21 19:16:11 +0800

    2 files modified

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 // Import stylesheets
 import './style.css';
 
+function init(){
+var sel = document.getElementById("studySelection");
+//var sel_id = sel.options[sel.selectedIndex].id;
+sel.addEventListener("change",function(){console.log(sel.value);});
+console.log(sel.value);
+};
+
+function change_form(){
+
+
+
+};
+
+
+function test(){
+console.log("file loaded");
+}
+
+window.init = init;
