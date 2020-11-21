a26d061d4ff0085f5834dbd07c2f57f27d616ac8
scsmss103 <s.schlumpf@gmail.com>
2020-11-21 20:00:04 +0800

    1 file modified

--- index.js
+++ index.js
@@ -10,7 +10,16 @@ console.log(sel.value);
 
 function change_form(){
 
+var forms = {
 
+inside_move:{
+  dit:{type:"number",id:"dit",name:"dit",value:"7",label:"Days in Trade"},
+  dte:{type:"number",id:"dte",name:"dte",value:"30",label:"Days to Expiry"},
+  cat:{type:"text",id:"cat",name:"name",value:"high_low",label:"Category",aria_describedby:"catHelp",description:"close or high_low"},
+  start:{type:"text",id:"start",name:"start",value:"2018-10-08",aria_describedby:"startHelp",description:"format: '2018-10-08' (default)"}
+}//inside_move
+
+}; //forms object
 
 };
 
